d6330e8c3a569f09ab1d4ff2dfd2a2f3778b5c2d
Volodymyr Sizmin <ukrmedias5@gmail.com>
2022-11-23 11:52:25 +0200

    Tests. Fixing mistakes

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -8,9 +8,7 @@ class UserService
 {
     public function createUser($data)
     {
-        $user = User::create($data);
-
-        return $user;
+        return User::create($data);
     }
 
 }
--- tests/Unit/UserServiceTest.php
+++ tests/Unit/UserServiceTest.php
@@ -3,14 +3,15 @@
 namespace Tests\Unit;
 
 use App\Services\UserService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class UserServiceTest extends TestCase
 {
+    use RefreshDatabase;
 
     public function test_create_user()
     {
-        $this->artisan('migrate');
         $this->artisan('passport:install');
 
         $create = new UserService();
